d428ee61da0bec1e3d680c8e3994186b7b97ce76
Lee eunji <144212524+eeeeeuuuuunnnnnjjjjjiiiii@users.noreply.github.com>
2024-02-15 01:44:22 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -2,34 +2,131 @@
 // + 버튼을 클릭하면 할일이 추가된다
 // 유저가 delete버튼을 누르면 할일이 삭제된다
 //check버튼을 누르면 할일이 끝나면서 밑줄이 간다
+//1.check버튼을 클릭하는 순간 true false
+//2. true이면 끝난걸로 간주하고 밑줄 보여주기
+//3.false이면 안끝난걸로 간주하고 그대로
 //진행중 끝남 탭을 누르면 언더바가 이동한다
 //끝남탭은 끝난 아이템만 진행중탭은 진행중인 아이템만나온다
 //전체탭을 누르면 다시 전체아이템으로 돌아옴
+//할일을 입력하고나면 입력창이 자동으로 비워지는 로직을 추가하셨으면 좋겠습니다!
+//입력한 할일이 없다면 추가가 안되게 막게 하는 기능도 추가해주세요! 
+// 버튼을 disable 시키던가 아니면 에러메세지 보여주는 형식으로
 
-let taskInput = document.getElementById("task-input");
-let addBtn = document.getElementById("add-button");
+let userInput = document.querySelector(".task-input");
+let addButton = document.querySelector(".button-add");
+let tabs = document.querySelectorAll(".tab-type div");
+let underLine = document.getElementById("tab-underline");
 let taskList = [];
+let mode = "all";
+let filterList = [];
 
-addBtn.addEventListener("click",addTask);
+addButton.addEventListener("mousedown", addTask);
+userInput.addEventListener("keydown", function (event) {
+  if (event.keyCode === 13) {
+    addTask(event);
+  }
+});
+for (let i = 0; i < tabs.length; i++) {
+  tabs[i].addEventListener("click", function (event) {
+    filter(event);
+  });
+}
 
-function addTask(){
-    let taskContent = taskInput.value;
-    taskList.push(taskContent);
-    console.log(taskList);
-    render();
-} 
+function addTask() {
+  let taskValue = userInput.value;
+  if (taskValue === "") return alert("할일을 입력해주세요");
+  let task = {
+    content: taskValue,
+    isComplete: false,
+    id: randomIDGenerator(),
+  };
 
-function render(){
-    let resultHTML = '';
-    for(let i = 0; i < taskList.length; i++){
-        resultHTML += `<div class="task">
-        <div>${taskList[i]}</div>
-         <div>
-             <button>check</button>
-             <button>delete</button>
-         </div>
-     </div>`;
+  taskList.push(task);
+  userInput.value = "";
+  render();
+}
+
+function render() {
+  let result = "";
+  list = [];
+  if (mode === "all") {
+    list = taskList;
+  } else {
+    // else if(mode=== "ongoing" || mode==="done") 을 결국 else 로 치환할 수 있다.
+    list = filterList;
+  }
+
+  for (let i = 0; i < list.length; i++) {
+    if (list[i].isComplete) {
+      result += `<div class="task task-done" id="${list[i].id}">
+            <span>${list[i].content}</span>
+            <div class="button-box">
+            <button onclick="toggleDone('${list[i].id}')"><i class="fas fa-undo-alt"></i></button>
+            <button onclick="deleteTask('${list[i].id}')"><i class="fa fa-trash"></i></button>
+            </div>
+        </div>`;
+    } else {
+      result += `<div class="task" id="${list[i].id}" >
+            <span>${list[i].content}</span>
+            <div class="button-box">
+            <button onclick="toggleDone('${list[i].id}')"><i class="fa fa-check"></i></button>
+            <button onclick="deleteTask('${list[i].id}')"><i class="fa fa-trash"></i></button>
+            </div>
+        </div>`;
     }
+  }
 
-    document.getElementById("task-board").innerHTML = resultHTML;
+  document.getElementById("task-board").innerHTML = result;
+}
+
+function toggleDone(id) {
+  for (let i = 0; i < taskList.length; i++) {
+    if (taskList[i].id === id) {
+      taskList[i].isComplete = !taskList[i].isComplete;
+      break;
+    }
+  }
+  filter();
+}
+
+function deleteTask(id) {
+  for (let i = 0; i < taskList.length; i++) {
+    if (taskList[i].id === id) {
+      taskList.splice(i, 1);
+    }
+  }
+
+  filter();
+}
+function filter(e) {
+  if (e) {
+    mode = e.target.id;
+    underLine.style.width = e.target.offsetWidth + "px";
+    underLine.style.left = e.target.offsetLeft + "px";
+    underLine.style.top =
+      e.target.offsetTop + (e.target.offsetHeight - 4) + "px";
+  } // 진행중 상태에서 끝남으로 표시하면 바로 사라지는 부분은 event가 없음 그래서 조건추가
+
+  filterList = [];
+  if (mode === "ongoing") {
+    for (let i = 0; i < taskList.length; i++) {
+      if (taskList[i].isComplete == false) {
+        filterList.push(taskList[i]);
+      }
+    }
+  } else if (mode === "done") {
+    for (let i = 0; i < taskList.length; i++) {
+      if (taskList[i].isComplete) {
+        filterList.push(taskList[i]);
+      }
+    }
+  }
+  render();
+}
+
+function randomIDGenerator() {
+  // Math.random should be unique because of its seeding algorithm.
+  // Convert it to base 36 (numbers + letters), and grab the first 9 characters
+  // after the decimal.
+  return "_" + Math.random().toString(36).substr(2, 9);
 }
